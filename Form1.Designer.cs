036f04799339a9ef54e945e745195d857377bf23
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-03-28 16:11:19 -0700

    Concluido

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace Actividad_Integradora_2_Problema_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAlfabetico = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPrecio = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAlfabetico
+            // 
+            this.btnAlfabetico.Location = new System.Drawing.Point(166, 134);
+            this.btnAlfabetico.Name = "btnAlfabetico";
+            this.btnAlfabetico.Size = new System.Drawing.Size(188, 58);
+            this.btnAlfabetico.TabIndex = 0;
+            this.btnAlfabetico.Text = "Aceptar";
+            this.btnAlfabetico.UseVisualStyleBackColor = true;
+            this.btnAlfabetico.Click += new System.EventHandler(this.btnAlfabetico_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Acomodo Alfabetico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(673, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Acomodo por precio";
+            // 
+            // btnPrecio
+            // 
+            this.btnPrecio.Location = new System.Drawing.Point(718, 134);
+            this.btnPrecio.Name = "btnPrecio";
+            this.btnPrecio.Size = new System.Drawing.Size(188, 58);
+            this.btnPrecio.TabIndex = 3;
+            this.btnPrecio.Text = "Aceptar";
+            this.btnPrecio.UseVisualStyleBackColor = true;
+            this.btnPrecio.Click += new System.EventHandler(this.btnPrecio_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Precio});
+            this.dgvDatos.Location = new System.Drawing.Point(252, 252);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 102;
+            this.dgvDatos.RowTemplate.Height = 49;
+            this.dgvDatos.Size = new System.Drawing.Size(600, 617);
+            this.dgvDatos.TabIndex = 4;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 12;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 250;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 12;
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            this.Precio.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1138, 1010);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAlfabetico);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAlfabetico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPrecio;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,82 @@ namespace Actividad_Integradora_2_Problema_3
 {
     public partial class Form1 : Form
     {
+        string[] nombre = new string[] { "Estufa", "Licuadora", "Refrigerador", "Plancha", "Horno Microondas", "Braum", 
+            "Cafetera", "Cuchillos", "Set de Cocina", "Vasos" };
+        double[] precio = new double[] { 5500.50, 1200.00, 12500.99, 250.90, 2500.00, 120.00, 450.50, 50.99, 3000.00, 20.00};
         public Form1()
         {
             InitializeComponent();
+            dgvDatos.Rows.Clear();
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                dgvDatos.Rows.Add(1);
+                dgvDatos.Rows[i].Cells[0].Value = nombre[i];
+                dgvDatos.Rows[i].Cells[1].Value = precio[i].ToString(); ;
+            }
         }
 
+        private void btnAlfabetico_Click(object sender, EventArgs e)
+        {
+            int length = nombre.Length;
+            dgvDatos.Rows.Clear();
+
+            for (int i = 0; i < length -1; i++)
+            {
+                for (int x = 0; x < length - i -1; x++)
+                {
+                    if (nombre[x].CompareTo(nombre[x+1])>0)
+                    {
+                        string auxNombre;
+                        auxNombre = nombre[x];
+                        nombre[x] = nombre[x + 1];
+                        nombre[x + 1] = auxNombre;
+                        double auxPrecio;
+                        auxPrecio = precio[x];
+                        precio[x] = precio[x + 1];
+                        precio[x + 1] = auxPrecio; 
+                    }
+                }
+            }
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                dgvDatos.Rows.Add(1);
+                dgvDatos.Rows[i].Cells[0].Value = nombre[i].ToString();
+                dgvDatos.Rows[i].Cells[1].Value = precio[i].ToString();
+            }
+        }
+
+        private void btnPrecio_Click(object sender, EventArgs e)
+        {
+            int length = precio.Length;
+            dgvDatos.Rows.Clear();
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int x = 0; x < length - i - 1; x++)
+                {
+                    if (precio[x] > precio[x + 1])
+                    {
+                        double auxPrecio;
+                        auxPrecio = precio[x];
+                        precio[x] = precio[x + 1];
+                        precio[x + 1] = auxPrecio;
+
+                        string auxNombre;
+                        auxNombre = nombre[x];
+                        nombre[x] = nombre[x + 1];
+                        nombre[x + 1] = auxNombre;
+                    }
+                }
+            }
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                dgvDatos.Rows.Add(1);
+                dgvDatos.Rows[i].Cells[0].Value = nombre[i].ToString();
+                dgvDatos.Rows[i].Cells[1].Value = precio[i].ToString();
+            }
+        }
     }
 }
